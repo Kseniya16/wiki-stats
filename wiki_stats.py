b417cb4fefd2a8652f7258f406e715834c1023a6
Valerie1296 <valeriya1296@gmail.com>
2017-03-29 15:29:30 +0300

    changes

--- wiki_stats.py
+++ wiki_stats.py
@@ -20,38 +20,88 @@ class WikiGraph:
         print('Загружаю граф из файла: ' + filename)
 
         with open(filename) as f:
-            (n, _nlinks) = (0, 0) # TODO: прочитать из файла
-            
+            (n, _nlinks) = map(int, f.readline().split())
+
             self._titles = []
             self._sizes = array.array('L', [0]*n)
             self._links = array.array('L', [0]*_nlinks)
             self._redirect = array.array('B', [0]*n)
             self._offset = array.array('L', [0]*(n+1))
 
+            for i in range(n):
+                self._titles.append(f.readline().rstrip())
+                a,b,c = map(int,f.readline().split())
+                self._sizes[i] = a
+                self._redirect[i] = b
+
+                for j in range(c):
+                    self._links[self._offset[i]+j] = int(f.readline())
+
+                self._offset[i+1] = self._offset[i] + c
+
+
             # TODO: прочитать граф из файла
 
         print('Граф загружен')
 
     def get_number_of_links_from(self, _id):
-        pass
+        return (self._offset[_id+1]-self._offset[_id])
 
     def get_links_from(self, _id):
-        pass
+        return self._links[self._offset[_id]:self._offset[_id+1]]
 
     def get_id(self, title):
-        pass
+        return self._titles.index[title]
 
     def get_number_of_pages(self):
-        pass
+        return len(self._titles)
 
     def is_redirect(self, _id):
-        pass
+        return self._redirect[_id]
 
     def get_title(self, _id):
-        pass
+        return self._titles[_id]
 
     def get_page_size(self, _id):
-        pass
+        return self._sizes[_id]
+
+
+graph=WikiGraph()
+graph.load_from_file('wiki_small.txt')
+n=graph.get_number_of_pages()
+s=0
+Q=[]
+C=[]
+for i in range(n):
+    if graph.is_redirect(i)!=0:
+        s=s+1
+        C.append(graph.get_links_from(i))
+    else:
+
+    Q.append(graph.get_number_of_links_from(i))
+q1=min(Q)
+q2=Q.count(q1)
+q3=max(Q)
+q4=Q.count(Q)
+q5=graph.get_title(Q.index(q3))
+sr=statistics.mean(Q)
+otk=statistics.stdev(Q)
+for i in C:
+
+print('количество статей с перенаправлением',s,"\n",
+      'минимальное количество ссылок из статьи', q1, "\n",
+      'количество статей с минимальным количеством ссылок',q2,"\n",
+      'максимальное количество ссылок из статьи', q3,"\n",
+      'количество статей с максимальным количеством ссылок',q4, "\n",
+      'статья с наибольшим количеством ссылок', q5, "\n",
+      'среднее количество ссылок в статье', sr, '(',otk,')',"\n",
+      )
+
+
+
+
+
+
 
 
 def hist(fname, data, bins, xlabel, ylabel, title, facecolor='green', alpha=0.5, transparent=True, **kwargs):
